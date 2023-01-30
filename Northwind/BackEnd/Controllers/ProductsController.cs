acaf78dda5b913ea51a1772cca0a4dc96494d2df
jgarro50028 <80930919+jgarro06@users.noreply.github.com>
2023-01-29 20:39:52 -0600

    creacion de API products

---
+++ Northwind/BackEnd/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private IProductsDAL productsDAL;
+
+        #region Constructor
+        public ProductsController()
+        {
+            productsDAL = new ProductsDALImpl(new Entities.Entities.NorthWindContext());
+        }
+
+        #endregion
+
+        #region Consultar
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<Product> products = productsDAL.GetAll();
+
+
+            return new JsonResult(products);
+        }
+                
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Product product;
+            product = productsDAL.Get(id);
+
+
+            return new JsonResult(product);
+        }
+
+        #endregion
+
+        #region  Agregar
+        // POST api/<ProductsController>
+        [HttpPost]
+        public JsonResult Post([FromBody] Product products)
+        {
+            productsDAL.Add(products);
+            return new JsonResult(products);
+        }
+
+        #endregion
+
+        #region Modificar
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public JsonResult Put([FromBody] Product products)
+        {
+            productsDAL.Update(products);
+            return new JsonResult(products);
+        }
+
+        #endregion
+
+        #region Eliminar
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            Product products = new Product { ProductId = id };
+            productsDAL.Remove(products);
+
+            return new JsonResult(products);
+        }
+
+        #endregion
+    }
+}
---
+++ Northwind/DAL/Implementations/ProductsDALImpl.cs
@@ -0,0 +1,142 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class ProductsDALImpl : IProductsDAL
+    {
+        NorthWindContext context;
+
+
+        public ProductsDALImpl()
+        {
+            context = new NorthWindContext();
+
+        }
+
+        public ProductsDALImpl(NorthWindContext _Context)
+        {
+            context = _Context;
+
+        }
+
+        public bool Add(Product entity)
+        {
+            try
+            {
+                using (UnidadDeTrabajo<Product> unidad = new UnidadDeTrabajo<Product>(context))
+                {
+                    unidad.genericDAL.Add(entity);
+                    unidad.Complete();
+                }
+
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> Find(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            Product products;
+            using (UnidadDeTrabajo<Product> unidad = new UnidadDeTrabajo<Product>(context))
+            {
+
+                products = unidad.genericDAL.Get(id);
+            }
+            return products;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            try
+            {
+                IEnumerable<Product> products;
+                using (UnidadDeTrabajo<Product> unidad = new UnidadDeTrabajo<Product>(context))
+                {
+                    products = unidad.genericDAL.GetAll();
+                }
+                return products;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Remove(Product entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<Product> unidad = new UnidadDeTrabajo<Product>(context))
+                {
+                    unidad.genericDAL.Remove(entity);
+                    result = unidad.Complete();
+                }
+            }
+            catch (Exception)
+            {
+
+                result = false;
+            }
+
+            return result;
+        
+    }
+
+        public void RemoveRange(IEnumerable<Product> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product SingleOrDefault(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Product entity)
+        {
+            bool result = false;
+
+            try
+            {
+                using (UnidadDeTrabajo<Product> unidad = new UnidadDeTrabajo<Product>(context))
+                {
+                    unidad.genericDAL.Update(entity);
+                    result = unidad.Complete();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return result;
+        }
+    }
+    
+}
