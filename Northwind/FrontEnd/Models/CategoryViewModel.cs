0dec3cbae371551cd4de3fa356226ea651d19982
jgarro50028 <80930919+jgarro06@users.noreply.github.com>
2023-02-12 17:28:46 -0600

    semana 3

---
+++ Northwind/FrontEnd/Models/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models
+{
+    public class CategoryViewModel
+    {
+
+        public int CategoryID { get; set; }
+        [Display(Name = "Categoría")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+    }
+}
